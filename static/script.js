db7f998e0ba0f212505ca43814c9bffbd8f2e436
Kahramon <kahramonhadjibaev@gmail.com>
2021-01-04 01:46:13 +0300

    first commit

---
+++ static/script.js
@@ -0,0 +1,8 @@
+// document.addEventListener('DOMContentLoaded', function (){
+// 	particlesJS.load('particles-js','particles.json');
+// 	})
+// const server = require('http').createServer();
+// const port = process.env.PORT || 3000;
+// server.listen(port, () => console.log(`Listening on ${port}`));
+"use strict";
+//# sourceMappingURL=script.js.map
